605628a4277d00540b04a925a7e1d25f17410f9f
Satish Kushwah <satishkushwahdigital@gmail.com>
2022-09-07 09:19:16 +0530

    change password implemented

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from home import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('home', views.home, name="home"),
-    path('logout', views.logoutUser, name="logout")
+    path('logout', views.logoutUser, name="logout"),
+    path('change_password', views.change_password, name="change_password")
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -32,3 +33,16 @@ def home(request):
 def logoutUser(request):
     logout(request)
     return redirect('/')
+
+
+def change_password(request):
+    if request.user.is_anonymous:
+        return redirect('/')
+    if request.method == "POST":
+        pwd = request.POST.get('pwd1')
+        u = User.objects.get(username=request.user)
+        u.set_password(pwd)
+        u.save()
+        messages.success(request, 'password changed')
+        return render(request, 'change_password.html')
+    return render(request, 'change_password.html')
